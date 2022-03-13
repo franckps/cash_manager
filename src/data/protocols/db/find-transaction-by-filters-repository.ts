94cb0664beb256109fd7f350e6119198288e8d4b
Francisco Pereira <ZNacJSmJqy7YF58arS6C>
2022-03-13 04:49:02 -0300

    fix: change the repository name

--- src/data/protocols/db/find-transaction-by-filters-repository.ts
+++ src/data/protocols/db/find-transaction-by-filters-repository.ts
@@ -1,6 +1,6 @@
 import { TransactionModel } from "../../../domain/models/transaction";
 import { TransactionFiltersModel } from "src/domain/usecases/find-transaction-by-filters";
 
-export interface FindTransactionByFilters {
+export interface FindTransactionByFiltersRepository {
   find(filters: TransactionFiltersModel): Promise<TransactionModel[]>;
 }
