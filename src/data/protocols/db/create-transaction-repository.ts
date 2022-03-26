a61b190bd6d88768b654ea19f1157de04c8a42c3
Francisco Pereira <ZNacJSmJqy7YF58arS6C>
2022-03-25 23:59:20 -0300

    refactor: change the CreateTransactionRepository create method to receive and return the same model

--- src/data/protocols/db/create-transaction-repository.ts
+++ src/data/protocols/db/create-transaction-repository.ts
@@ -1,6 +1,5 @@
 import { TransactionModel } from "../../../domain/models/transaction";
-import { CreateTransactionModel } from "../../../domain/usecases/create-transaction";
 
 export interface CreateTransactionRepository {
-  create(transaction: CreateTransactionModel): Promise<TransactionModel>;
+  create(transaction: TransactionModel): Promise<TransactionModel>;
 }
