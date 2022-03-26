c69bb97b58ef8954506010b50025043dca84519c
Francisco Pereira <ZNacJSmJqy7YF58arS6C>
2022-03-26 00:00:54 -0300

    test: add the unique id and the status generation into the DbCreateTransaction class

--- tests/data/usecases/db-create-transaction.spec.ts
+++ tests/data/usecases/db-create-transaction.spec.ts
@@ -2,34 +2,81 @@ import { DbCreateTransaction } from "../../../src/data/usecases/db-create-transa
 import { CreateTransactionRepository } from "../../../src/data/protocols/db/create-transaction-repository";
 import { TransactionModel } from "../../../src/domain/models/transaction";
 import { CreateTransactionModel } from "../../../src/domain/usecases/create-transaction";
+import crypto from "crypto";
 
-describe("DbCreateTransaction Usecase", () => {
-  test("Should call CreateTransactionRepository with the correct values", async () => {
-    class CreateTransactionRepositoryStub
-      implements CreateTransactionRepository
-    {
-      constructor() {}
-      create(transaction: CreateTransactionModel): Promise<TransactionModel> {
-        return null as any;
-      }
+const makeTransactionData = (): {
+  amount: string;
+  type: "Receipt" | "Payment";
+  title: string;
+  description: string;
+} => ({
+  amount: "any_amount",
+  type: "Payment",
+  title: "any_title",
+  description: "any_description",
+});
+
+const makeSut = (): {
+  sut: DbCreateTransaction;
+  createTransactionRepositoryStub: CreateTransactionRepository;
+} => {
+  class CreateTransactionRepositoryStub implements CreateTransactionRepository {
+    constructor() {}
+    create(transaction: TransactionModel): Promise<TransactionModel> {
+      return Promise.resolve(transaction);
     }
-    const createTransactionRepositoryStub =
-      new CreateTransactionRepositoryStub();
-    const sut = new DbCreateTransaction(createTransactionRepositoryStub);
+  }
+  const createTransactionRepositoryStub = new CreateTransactionRepositoryStub();
+  const sut = new DbCreateTransaction(createTransactionRepositoryStub);
 
+  return { sut, createTransactionRepositoryStub };
+};
+
+describe("DbCreateTransaction UseCase", () => {
+  const spyRandomUUID = jest
+    .spyOn(crypto, "randomUUID")
+    .mockReturnValue("any_randomUUID");
+  test("Should call the crypto randomUUID method to generate an unique id", async () => {
+    const { sut } = makeSut();
+    await sut.create(makeTransactionData());
+
+    expect(spyRandomUUID).toHaveBeenCalled();
+  });
+  test("Should call CreateTransactionRepository with the correct values", async () => {
+    const { sut, createTransactionRepositoryStub } = makeSut();
     const createSpy = jest.spyOn(createTransactionRepositoryStub, "create");
-    await sut.create({
-      amount: "any_amount",
-      type: "Payment",
-      title: "any_title",
-      description: "any_description",
-    });
+    await sut.create(makeTransactionData());
 
     expect(createSpy).toHaveBeenCalledWith({
+      _id: "any_randomUUID",
       amount: "any_amount",
       type: "Payment",
       title: "any_title",
       description: "any_description",
+      status: "active",
+    });
+  });
+  test("Should throw if CreateTransactionRepository throws", async () => {
+    const { sut, createTransactionRepositoryStub } = makeSut();
+    jest
+      .spyOn(createTransactionRepositoryStub, "create")
+      .mockRejectedValue(new Error("any_error"));
+    jest.spyOn(sut, "create");
+    const promiseRejected = sut.create(makeTransactionData());
+
+    await expect(promiseRejected).rejects.toThrow();
+  });
+  test("Should return the correct data if succeeded", async () => {
+    const { sut } = makeSut();
+    const newTransaction = await sut.create(makeTransactionData());
+
+    expect(newTransaction).toEqual({
+      _id: "any_randomUUID",
+      amount: "any_amount",
+      type: "Payment",
+      title: "any_title",
+      description: "any_description",
+      status: "active",
     });
   });
 });
