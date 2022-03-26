89738c042016219a56c1985a04cbdd2f21f06972
Francisco Pereira <ZNacJSmJqy7YF58arS6C>
2022-03-26 00:44:42 -0300

    feat: create the DbFindTransactionByFilters class

---
+++ src/data/usecases/db-find-transaction-by-filters.ts
@@ -0,0 +1,17 @@
+import { TransactionModel } from "src/domain/models/transaction";
+import {
+  FindTransactionByFilters,
+  TransactionFiltersModel,
+} from "../../domain/usecases/find-transaction-by-filters";
+import { FindTransactionByFiltersRepository } from "../protocols/db/find-transaction-by-filters-repository";
+
+export class DbFindTransactionByFilters implements FindTransactionByFilters {
+  constructor(
+    private readonly repository: FindTransactionByFiltersRepository
+  ) {}
+  async find(
+    transaction: TransactionFiltersModel
+  ): Promise<TransactionModel[]> {
+    return await this.repository.find(transaction);
+  }
+}
