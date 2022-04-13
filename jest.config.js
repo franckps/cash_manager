97fa62de44317c77908d7603c835a4634de7f755
Francisco Pereira <fran.p.s@hotmail.com>
2022-04-13 00:00:29 -0300

    config: remove the coverage collection from interface files

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   roots: ["<rootDir>"],
   clearMocks: true,
-  collectCoverageFrom: ["<rootDir>/src/**/*.ts", "!<rootDir>/src/main/**"],
+  collectCoverageFrom: [
+    "<rootDir>/src/**/*.ts",
+    "!<rootDir>/src/domain/**",
+    "!<rootDir>/src/main/**",
+    "!<rootDir>/src/**/protocols/**",
+  ],
   coverageDirectory: "coverage",
   coverageProvider: "v8",
   testEnvironment: "node",
