9ebd42ba8c75c1973243cb78e5362b338d3b6114
franckps <fran.p.s@hotmail.com>
2024-02-25 02:30:35 -0300

    test: create a factory to build FindTransactionByFiltersController class

--- src/infra/factories/controller-factory.ts
+++ src/infra/factories/controller-factory.ts
@@ -1,9 +1,14 @@
 import { CreateTransactionController } from "../../presentation/controllers/create-transaction-controller";
 import { TransactionRepository } from "../db/sequelize/transaction-repository";
 import { Transaction } from "../db/sequelize/sequelize-instance";
+import { FindTransactionByFiltersController } from "../../presentation/controllers/index-transaction-controller";
 
 const transactionRepository = new TransactionRepository(Transaction);
 
 export const buildCreateTransactionController = () => {
   return new CreateTransactionController(transactionRepository);
+};
+
+export const buildFindTransactionByFiltersController = () => {
+  return new FindTransactionByFiltersController(transactionRepository);
 };
