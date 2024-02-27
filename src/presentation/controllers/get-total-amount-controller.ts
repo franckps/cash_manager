254b19b570480b493434da823445be727e1e02fa
franckps <fran.p.s@hotmail.com>
2024-02-27 20:48:08 -0300

    feat: create controller to get total transaction amount

---
+++ src/presentation/controllers/get-total-amount-controller.ts
@@ -0,0 +1,16 @@
+import { GetTotalAmount } from "src/domain/usecases/get-total-amount";
+import { Controller } from "../protocols/controller";
+import { HttpRequest, HttpResponse } from "../protocols/http";
+
+export class GetTotalAmountController implements Controller {
+  constructor(private readonly getTotalAmount: GetTotalAmount) {}
+
+  async handle(_: HttpRequest): Promise<HttpResponse> {
+    const result = await this.getTotalAmount.get();
+
+    return {
+      statusCode: 200,
+      body: result,
+    };
+  }
+}
