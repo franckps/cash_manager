f9378a25a0364311acfc7243065238c93e7b67c0
franckps <fran.p.s@hotmail.com>
2024-02-28 22:31:38 -0300

    test: create revert transaction controller

---
+++ tests/presentation/revert-transaction-controller.spec.ts
@@ -0,0 +1,53 @@
+import { RevertTransaction } from "../../src/domain/usecases/revert-transaction";
+import { RevertTransactionController } from "../../src/presentation/controllers/revert-transaction-controller";
+
+interface SutTypes {
+  sut: RevertTransactionController;
+  revertTransactionStub: RevertTransaction;
+}
+
+const makeTransactionRequest = () => ({
+  params: { id: "123" },
+});
+
+const makeSut = (): SutTypes => {
+  class RevertTransactionStub implements RevertTransaction {
+    revert(_id: string): Promise<void> {
+      return Promise.resolve();
+    }
+  }
+  const revertTransactionStub = new RevertTransactionStub();
+  const sut = new RevertTransactionController(revertTransactionStub);
+
+  return {
+    sut,
+    revertTransactionStub,
+  };
+};
+
+describe("RevertTransactionController", () => {
+  test("should call RevertTransaction correctly", async () => {
+    const { sut, revertTransactionStub } = makeSut();
+    const revertSpy = jest.spyOn(revertTransactionStub, "revert");
+    sut.handle(makeTransactionRequest());
+
+    expect(revertSpy).toHaveBeenCalledWith("123");
+  });
+
+  test("should throw if RevertTransaction throws", async () => {
+    const { sut, revertTransactionStub } = makeSut();
+    jest
+      .spyOn(revertTransactionStub, "revert")
+      .mockRejectedValue(new Error("any_error"));
+    const promiseRejected = sut.handle(makeTransactionRequest());
+
+    await expect(promiseRejected).rejects.toThrow(new Error("any_error"));
+  });
+
+  test("should return correct data", async () => {
+    const { sut } = makeSut();
+    const result = await sut.handle(makeTransactionRequest());
+
+    expect(result.body.success).toEqual(true);
+  });
+});
