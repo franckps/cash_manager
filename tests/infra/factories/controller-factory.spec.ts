bc2bd4c6d6db136699d1b38ad2e3d02166bf362a
franckps <fran.p.s@hotmail.com>
2024-03-01 22:48:28 -0300

    test: create factory to build find by id controller

--- tests/infra/factories/controller-factory.spec.ts
+++ tests/infra/factories/controller-factory.spec.ts
@@ -3,11 +3,13 @@ import {
   buildFindTransactionByFiltersController,
   buildGetTotalAmountController,
   buildRevertTransactionController,
+  buildFindTransactionByIdController,
 } from "../../../src/infra/factories/controller-factory";
 import { CreateTransactionController } from "../../../src/presentation/controllers/create-transaction-controller";
 import { FindTransactionByFiltersController } from "../../../src/presentation/controllers/index-transaction-controller";
 import { GetTotalAmountController } from "../../../src/presentation/controllers/get-total-amount-controller";
 import { RevertTransactionController } from "../../../src/presentation/controllers/revert-transaction-controller";
+import { FindTransactionByIdController } from "../../../src/presentation/controllers/find-transaction-by-id-controller";
 
 describe("Controller Factory", () => {
   afterAll(() => {
@@ -33,5 +35,10 @@ describe("Controller Factory", () => {
   test("#buildRevertTransactionController", () => {
     const controller = buildRevertTransactionController();
     expect(controller).toBeInstanceOf(RevertTransactionController);
+  });
+
+  test("#buildFindTransactionByIdController", () => {
+    const controller = buildFindTransactionByIdController();
+    expect(controller).toBeInstanceOf(FindTransactionByIdController);
   });
 });
