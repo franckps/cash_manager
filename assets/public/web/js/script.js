581268a303d3611686d8c68d3e5b61bbd3e017c4
franckps <fran.p.s@hotmail.com>
2024-03-09 09:37:55 -0300

    feat: add filter field feature

--- assets/public/web/js/script.js
+++ assets/public/web/js/script.js
@@ -1,63 +1,84 @@
 const transactionData = {
-  updateCallbacks: [],
-  subscribeUpdate: function (callback) {
-    this.updateCallbacks.push(callback);
+  data: [],
+  filterData: [],
+  filterString: "",
+  update: async function () {
+    this.data = await this.fetchTransactions();
+    this.filter();
   },
-  update: function () {
-    this.updateCallbacks.forEach((cbk) => {
-      cbk.apply(null);
+  filter: function () {
+    this.updateFilterString();
+
+    this.filterData = [];
+
+    if (!this.filterString || this.filterString == "") {
+      this.filterData = this.data;
+      this.renderList();
+      return;
+    }
+
+    this.data.forEach((element) => {
+      const valoresJoined = Object.values(element).join(" ").toLowerCase();
+      const strFilter = this.filterString.toLowerCase();
+      console.log({ strFilter });
+      console.log({ valoresJoined });
+      if (new RegExp(`.*${strFilter}.*`).test(valoresJoined))
+        this.filterData.push(element);
     });
+    this.renderList();
+  },
+  renderList: function () {
+    const mainContent = document.getElementById("content");
+    console.log(this.filterData);
+    let itemsHTML = this.filterData
+      .map((element) => this.generateItemHTML(element))
+      .join("");
+    mainContent.innerHTML = itemsHTML;
+  },
+  generateItemHTML: function (itemData) {
+    let dateString = itemData.createdAt.split(" +00:00")[0];
+    console.log();
+    const datetime = new Date(dateString);
+    console.log({ datetime });
+    const [day, month, year, hour, minutes, secounds] = [
+      datetime.getDay(),
+      datetime.getMonth(),
+      datetime.getFullYear(),
+      datetime.getHours(),
+      datetime.getMinutes(),
+      datetime.getSeconds(),
+    ];
+
+    return `
+        <section class="content-item ${
+          itemData.status == "reverted" ? "reverted" : ""
+        }">
+            <p>${itemData.title}</p>
+            <p><span class="${itemData.type.toLowerCase()}">${
+      itemData.type == "Receipt" ? "ENTRADA" : "SAÍDA"
+    }</span></p>
+            <p>${day}/${month + 1}/${year} ${hour}:${minutes}:${secounds}</p>
+            <p>${itemData.description}</p>
+            <p>R$ ${itemData.amount}</p>
+            <p>
+              ${
+                itemData.status == "active"
+                  ? "<button class='btn-revert'><span>⨁</span> Extornar</button>"
+                  : ""
+              }
+            </p>
+        </section>
+    `;
+  },
+  updateFilterString: function () {
+    const filterField = document.getElementById("filter");
+    this.filterString = filterField.value;
+  },
+  fetchTransactions: async function () {
+    const request = await fetch("/api/v1/");
+    const result = await request.json();
+    return result.body;
   },
 };
-
-const fetchTransactions = async () => {
-  const request = await fetch("/api/v1/");
-  const result = await request.json();
-  return result.body;
-};
-
-const generateItemHTML = (itemData) => {
-  let dateString = itemData.createdAt.split(" +00:00")[0];
-  console.log();
-  const datetime = new Date(dateString);
-  console.log({ datetime });
-  const [day, month, year, hour, minutes, secounds] = [
-    datetime.getDay(),
-    datetime.getMonth(),
-    datetime.getFullYear(),
-    datetime.getHours(),
-    datetime.getMinutes(),
-    datetime.getSeconds(),
-  ];
-
-  return `
-      <section class="content-item ${
-        itemData.status == "reverted" ? "reverted" : ""
-      }">
-          <p>${itemData.title}</p>
-          <p><span class="${itemData.type.toLowerCase()}">${
-    itemData.type == "Receipt" ? "ENTRADA" : "SAÍDA"
-  }</span></p>
-          <p>${day}/${month + 1}/${year} ${hour}:${minutes}:${secounds}</p>
-          <p>${itemData.description}</p>
-          <p>R$ ${itemData.amount}</p>
-          <p>
-            ${
-              itemData.status == "active"
-                ? "<button class='btn-revert'><span>⨁</span> Extornar</button>"
-                : ""
-            }
-          </p>
-      </section>
-  `;
-};
-
-transactionData.subscribeUpdate(async () => {
-  let data = await fetchTransactions();
-
-  const mainContent = document.getElementById("content");
-  let itemsHTML = data.map((element) => generateItemHTML(element)).join("");
-  mainContent.innerHTML = itemsHTML;
-});
 
 transactionData.update();
