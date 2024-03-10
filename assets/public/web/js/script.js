4ce9e81135e3b9366024a2ffea2a8f0ea3071de7
franckps <fran.p.s@hotmail.com>
2024-03-10 11:03:45 -0300

    feat: make sort buttons work

--- assets/public/web/js/script.js
+++ assets/public/web/js/script.js
@@ -2,6 +2,7 @@ const transactionData = {
   data: [],
   filterData: [],
   filterString: "",
+  sortArray: ["", ""],
   update: async function () {
     this.data = await this.fetchTransactions();
     this.filter();
@@ -30,8 +31,6 @@ const transactionData = {
       valoresJoined = valoresJoined.toLowerCase();
       const strFilter = this.filterString.toLowerCase();
 
-      console.log({ strFilter });
-      console.log({ valoresJoined });
       if (new RegExp(`.*${strFilter}.*`).test(valoresJoined))
         this.filterData.push(element);
     });
@@ -39,7 +38,6 @@ const transactionData = {
   },
   renderList: function () {
     const mainContent = document.getElementById("content");
-    console.log(this.filterData);
     let itemsHTML = this.filterData
       .map((element) => this.generateItemHTML(element))
       .join("");
@@ -48,7 +46,7 @@ const transactionData = {
   formatDate: function (stringDate) {
     let dateString = stringDate.split(" +00:00")[0];
     const datetime = new Date(dateString);
-    console.log({ datetime });
+
     const [day, month, year, hour, minutes, secounds] = [
       datetime.getDay(),
       datetime.getMonth(),
@@ -96,6 +94,28 @@ const transactionData = {
     const request = await fetch("/api/v1/");
     const result = await request.json();
     return result.body;
+  },
+  sort: function (column /** column name */, ref /** ASC | DES */ = "asc") {
+    if (!!column) this.sortArray = [column, ref.toLowerCase()];
+    if (!this.sortArray[0]) {
+      this.filter();
+      return;
+    }
+
+    let aux = this.data.sort((first, other) => {
+      result =
+        first[column] > other[column]
+          ? -1
+          : first[column] < other[column]
+          ? 1
+          : 0;
+      if (this.sortArray[1] == "asc") result = result * -1;
+
+      return result;
+    });
+
+    this.data = aux;
+    this.filter();
   },
 };
 
