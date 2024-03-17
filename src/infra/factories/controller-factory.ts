cbeca524bcf5be75c44822b724037a46ef500100
franckps <fran.p.s@hotmail.com>
2024-03-17 00:20:56 -0300

    feat: create a factory to build controller to delete transaction

--- src/infra/factories/controller-factory.ts
+++ src/infra/factories/controller-factory.ts
@@ -5,9 +5,11 @@ import { FindTransactionByFiltersController } from "../../presentation/controlle
 import { DbCreateTransaction } from "../../data/usecases/db-create-transaction";
 import { DbFindTransactionByFilters } from "../../data/usecases/db-find-transaction-by-filters";
 import { DbGetTotalAmount } from "../../data/usecases/db-get-total-amount";
-import { GetTotalAmountController } from "../../presentation/controllers/get-total-amount-controller";
 import { DbRevertTransaction } from "../../data/usecases/db-revert-transaction";
+import { DbDeleteTransaction } from "../../data/usecases/db-delete-transaction";
+import { GetTotalAmountController } from "../../presentation/controllers/get-total-amount-controller";
 import { RevertTransactionController } from "../../presentation/controllers/revert-transaction-controller";
+import { DeleteTransactionController } from "../../presentation/controllers/delete-transaction-controller";
 import { FindTransactionByIdController } from "../../presentation/controllers/find-transaction-by-id-controller";
 import { DbFindTransactionById } from "../../data/usecases/db-find-transaction-by-id";
 
@@ -40,5 +42,11 @@ export const buildRevertTransactionController = () => {
 export const buildFindTransactionByIdController = () => {
   return new FindTransactionByIdController(
     new DbFindTransactionById(transactionRepository)
+  );
+};
+
+export const buildDeleteTransactionController = () => {
+  return new DeleteTransactionController(
+    new DbDeleteTransaction(transactionRepository)
   );
 };
