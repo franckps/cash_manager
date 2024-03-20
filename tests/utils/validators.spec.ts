fa2b27c5920997b8a64ee91356512533fa374ef0
franckps <fran.p.s@hotmail.com>
2024-03-19 21:27:43 -0300

    test: apply the validator protocol to create transaction validator

--- tests/utils/validators.spec.ts
+++ tests/utils/validators.spec.ts
@@ -1,55 +1,57 @@
 import { CreateTransactionModel } from "../../src/domain/usecases/create-transaction";
-import { Validators } from "../../src/utils/validators";
+import { CreateTransactionValidator } from "../../src/utils/validators";
 
 describe("Validators", () => {
-  test("Should throw if transaction amount be empty", () => {
-    const validators = new Validators();
-    const transaction = {
-      amount: "",
-      type: "Payment",
-      title: "any_test",
-      description: "any_description",
-    } as CreateTransactionModel;
-    try {
-      validators.validCreateTransaction(transaction);
-    } catch (err) {
-      console.error(err);
-      return expect(true).toBeTruthy();
-    }
-    expect(false).toBeTruthy();
-  });
+  describe("CreateTransactionValidator", () => {
+    test("Should throw if transaction amount be empty", () => {
+      const createTransactionValidator = new CreateTransactionValidator();
+      const transaction = {
+        amount: "",
+        type: "Payment",
+        title: "any_test",
+        description: "any_description",
+      } as CreateTransactionModel;
+      try {
+        createTransactionValidator.validate(transaction);
+      } catch (err) {
+        console.error(err);
+        return expect(true).toBeTruthy();
+      }
+      expect(false).toBeTruthy();
+    });
 
-  test("Should throw if transaction type be empty", () => {
-    const validators = new Validators();
-    const transaction = {
-      amount: "1",
-      type: "" as any,
-      title: "any_test",
-      description: "any_description",
-    } as CreateTransactionModel;
-    try {
-      validators.validCreateTransaction(transaction);
-    } catch (err) {
-      console.error(err);
-      return expect(true).toBeTruthy();
-    }
-    expect(false).toBeTruthy();
-  });
+    test("Should throw if transaction type be empty", () => {
+      const createTransactionValidator = new CreateTransactionValidator();
+      const transaction = {
+        amount: "1",
+        type: "" as any,
+        title: "any_test",
+        description: "any_description",
+      } as CreateTransactionModel;
+      try {
+        createTransactionValidator.validate(transaction);
+      } catch (err) {
+        console.error(err);
+        return expect(true).toBeTruthy();
+      }
+      expect(false).toBeTruthy();
+    });
 
-  test("Should throw if transaction title be empty", () => {
-    const validators = new Validators();
-    const transaction = {
-      amount: "1",
-      type: "Payment",
-      title: "",
-      description: "any_description",
-    } as CreateTransactionModel;
-    try {
-      validators.validCreateTransaction(transaction);
-    } catch (err) {
-      console.error(err);
-      return expect(true).toBeTruthy();
-    }
-    expect(false).toBeTruthy();
+    test("Should throw if transaction title be empty", () => {
+      const createTransactionValidator = new CreateTransactionValidator();
+      const transaction = {
+        amount: "1",
+        type: "Payment",
+        title: "",
+        description: "any_description",
+      } as CreateTransactionModel;
+      try {
+        createTransactionValidator.validate(transaction);
+      } catch (err) {
+        console.error(err);
+        return expect(true).toBeTruthy();
+      }
+      expect(false).toBeTruthy();
+    });
   });
 });
