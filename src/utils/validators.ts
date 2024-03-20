0d2a69c02a7e07a7702ec0a49fff4ec153338675
franckps <fran.p.s@hotmail.com>
2024-03-19 21:27:20 -0300

    refactor: apply the validator protocol to create transaction validator

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -1,5 +1,6 @@
 import { z } from "zod";
 import { CreateTransactionModel } from "../domain/usecases/create-transaction";
+import { Validator } from "../presentation/protocols/validator";
 
 const transactionScheme = z.object({
   amount: z.coerce.number().gt(0),
@@ -7,8 +8,10 @@ const transactionScheme = z.object({
   title: z.string().nonempty(),
 });
 
-export class Validators {
-  public validCreateTransaction(transaction: CreateTransactionModel): void {
+export class CreateTransactionValidator
+  implements Validator<CreateTransactionModel>
+{
+  public validate(transaction: CreateTransactionModel): void {
     transactionScheme.parse(transaction);
   }
 }
