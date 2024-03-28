f21b915eb35d8661c3ebb20849ffb3fe95d7bde3
franckps <fran.p.s@hotmail.com>
2024-03-27 23:50:38 -0300

    test: add factory methods to build account controller

--- tests/infra/factories/controller-factory.spec.ts
+++ tests/infra/factories/controller-factory.spec.ts
@@ -5,6 +5,8 @@ import {
   buildRevertTransactionController,
   buildDeleteTransactionController,
   buildFindTransactionByIdController,
+  buildCreateAccountController,
+  buildDeleteAccountController,
 } from "../../../src/infra/factories/controller-factory";
 import { CreateTransactionController } from "../../../src/presentation/controllers/transaction/create-transaction-controller";
 import { FindTransactionByFiltersController } from "../../../src/presentation/controllers/transaction/index-transaction-controller";
@@ -12,6 +14,8 @@ import { GetTotalAmountController } from "../../../src/presentation/controllers/
 import { RevertTransactionController } from "../../../src/presentation/controllers/transaction/revert-transaction-controller";
 import { DeleteTransactionController } from "../../../src/presentation/controllers/transaction/delete-transaction-controller";
 import { FindTransactionByIdController } from "../../../src/presentation/controllers/transaction/find-transaction-by-id-controller";
+import { CreateAccountController } from "../../../src/presentation/controllers/account/create-account-controller";
+import { DeleteAccountController } from "../../../src/presentation/controllers/account/delete-account-controller";
 
 describe("Controller Factory", () => {
   afterAll(() => {
@@ -19,33 +23,47 @@ describe("Controller Factory", () => {
     jest.resetAllMocks();
   });
 
-  test("#buildCreateTransactionController", () => {
-    const controller = buildCreateTransactionController();
-    expect(controller).toBeInstanceOf(CreateTransactionController);
+  describe("Transaction", () => {
+    test("#buildCreateTransactionController", () => {
+      const controller = buildCreateTransactionController();
+      expect(controller).toBeInstanceOf(CreateTransactionController);
+    });
+
+    test("#buildFindTransactionByFiltersController", () => {
+      const controller = buildFindTransactionByFiltersController();
+      expect(controller).toBeInstanceOf(FindTransactionByFiltersController);
+    });
+
+    test("#buildGetTotalAmountController", () => {
+      const controller = buildGetTotalAmountController();
+      expect(controller).toBeInstanceOf(GetTotalAmountController);
+    });
+
+    test("#buildRevertTransactionController", () => {
+      const controller = buildRevertTransactionController();
+      expect(controller).toBeInstanceOf(RevertTransactionController);
+    });
+
+    test("#buildFindTransactionByIdController", () => {
+      const controller = buildFindTransactionByIdController();
+      expect(controller).toBeInstanceOf(FindTransactionByIdController);
+    });
+
+    test("#buildDeleteTransactionController", () => {
+      const controller = buildDeleteTransactionController();
+      expect(controller).toBeInstanceOf(DeleteTransactionController);
+    });
   });
 
-  test("#buildFindTransactionByFiltersController", () => {
-    const controller = buildFindTransactionByFiltersController();
-    expect(controller).toBeInstanceOf(FindTransactionByFiltersController);
-  });
+  describe("Account", () => {
+    test("#buildCreateAccountController", () => {
+      const controller = buildCreateAccountController();
+      expect(controller).toBeInstanceOf(CreateAccountController);
+    });
 
-  test("#buildGetTotalAmountController", () => {
-    const controller = buildGetTotalAmountController();
-    expect(controller).toBeInstanceOf(GetTotalAmountController);
-  });
-
-  test("#buildRevertTransactionController", () => {
-    const controller = buildRevertTransactionController();
-    expect(controller).toBeInstanceOf(RevertTransactionController);
-  });
-
-  test("#buildFindTransactionByIdController", () => {
-    const controller = buildFindTransactionByIdController();
-    expect(controller).toBeInstanceOf(FindTransactionByIdController);
-  });
-
-  test("#buildDeleteTransactionController", () => {
-    const controller = buildDeleteTransactionController();
-    expect(controller).toBeInstanceOf(DeleteTransactionController);
+    test("#buildDeleteAccountController", () => {
+      const controller = buildDeleteAccountController();
+      expect(controller).toBeInstanceOf(DeleteAccountController);
+    });
   });
 });
