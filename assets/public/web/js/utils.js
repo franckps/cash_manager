1d3860f9b973002490c15fd9a6187e62a7f277ac
franckps <fran.p.s@hotmail.com>
2024-04-10 22:46:31 -0300

    feat: change formatDate method to use locale datestring to return formatted date

--- assets/public/web/js/utils.js
+++ assets/public/web/js/utils.js
@@ -2,16 +2,7 @@ const formatDate = function (stringDate) {
   let dateString = stringDate.split(" +00:00")[0];
   const datetime = new Date(dateString);
 
-  const [day, month, year, hour, minutes, secounds] = [
-    datetime.getDate(),
-    datetime.getMonth(),
-    datetime.getFullYear(),
-    datetime.getHours(),
-    datetime.getMinutes(),
-    datetime.getSeconds(),
-  ];
-
-  return `${day}/${month + 1}/${year} ${hour}:${minutes}:${secounds}`;
+  return datetime.toLocaleDateString() + " " + datetime.toLocaleTimeString();
 };
 
 const formatType = function (type) {
